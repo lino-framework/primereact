7e91a85377e0c631275b3b2cc0c07dda10d0e3c5
Tonis Piip <Tonis@eggo.org>
2019-05-30 16:06:49 +0300

    Compiled changes

--- components/autocomplete/AutoComplete.js
+++ components/autocomplete/AutoComplete.js
@@ -239,9 +239,9 @@ function (_Component) {
         if (this.props.appendTo) {
           this.panel.element.style.minWidth = _DomHandler.default.getWidth(target) + 'px';
 
-          _DomHandler.default.absolutePosition(this.panel.element, target);
+          _DomHandler.default.absolutePosition(this.panel.element, target, this.container);
         } else {
-          _DomHandler.default.relativePosition(this.panel.element, target);
+          _DomHandler.default.relativePosition(this.panel.element, target, this.container);
         }
       }
     }
